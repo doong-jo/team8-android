300365b0413940bb97330c98d275b12ac2e10868
ddalf <twiidi_@naver.com>
2018-10-27 01:07:48 +0900

    Modify:login fragment

--- app/src/main/java/com/helper/helper/view/login/LoginFragment.java
+++ app/src/main/java/com/helper/helper/view/login/LoginFragment.java
@@ -12,7 +12,9 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -24,6 +26,7 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,11 +44,23 @@ import org.json.JSONObject;
 
 public class LoginFragment extends Fragment {
     private final static String TAG = LoginFragment.class.getSimpleName() + "/DEV";
+    private static final int EDITTEXT_CONTROL_CLEAR = 229;
+    private static final int EDITTEXT_CONTROL_CHECK = 230;
 
     /******************* Define widgtes in view *******************/
     private FloatingLabelEditText m_emailInput;
     private FloatingLabelEditText m_pwInput;
     private Button m_loginBtn;
+    private Button m_emailInputClear;
+    private Button m_pwInputClear;
+    private OnClickListener m_emailInputClearClickListener;
+    private OnClickListener m_pwInputClearClickListener;
+
+    private static final int MAX_EMAIL_LENGTH = 40;
+    private static final int MAX_PW_LENGTH = 15;
+
+    // save original pixel(after convert dp) of editText control marginEnd
+    private int m_editTextControlMarginEnd;
     /**************************************************************/
 
     public LoginFragment() {
@@ -60,13 +75,82 @@ public class LoginFragment extends Fragment {
         m_emailInput = view.findViewById(R.id.loginEmailInput);
         m_pwInput = view.findViewById(R.id.loginPwInput);
         m_loginBtn = view.findViewById(R.id.loginBtn);
+        m_emailInputClear = view.findViewById(R.id.loginEmailInput_clear);
+        m_pwInputClear = view.findViewById(R.id.loginPwInput_clear);
+
+        m_emailInput.getmEditText().setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        m_pwInput.getmEditText().setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        m_pwInput.getmEditText().setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+
+
+
+
         /*******************************************************************/
 
         /******************* Make Listener in View *******************/
+        m_emailInput.getmEditText().setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if( !hasFocus ) {
+                    m_emailInputClear.setVisibility(View.INVISIBLE);
+                    return;
+                }
 
+                if( !m_emailInput.getmEditText().getText().equals("") ) {
+                    m_emailInputClear.setOnClickListener(m_emailInputClearClickListener);
+                    setControlEditText(EDITTEXT_CONTROL_CLEAR, m_emailInputClear);
+                }
+            }
+
+        });
+
+        m_emailInput.getmEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if( m_emailInput.getText().length() > MAX_EMAIL_LENGTH) {
+                    String maximumAllowedCharacters = m_emailInput.getmEditText().getText().toString().substring(0, MAX_EMAIL_LENGTH);
+
+                    m_emailInput.getmEditText().setText(maximumAllowedCharacters);
+                    m_emailInput.getmEditText().setSelection(MAX_EMAIL_LENGTH);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if( !m_emailInput.getmEditText().getText().toString().equals("") ) {
+                    m_emailInputClear.setVisibility(View.VISIBLE);
+                } else {
+                    m_emailInputClear.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
         /*************************************************************/
 
         return view;
+    }
+
+    private void setControlEditText(int typeCode, Button control) {
+        control.setVisibility(View.VISIBLE);
+
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams)control.getLayoutParams();
+        final int marginEndDp = params.getMarginEnd();
+
+        if( typeCode == EDITTEXT_CONTROL_CLEAR ) {
+            control.setBackgroundResource(R.drawable.ic_delete);
+            params.setMarginEnd(m_editTextControlMarginEnd);
+            control.setLayoutParams(params);
+        } else if ( typeCode == EDITTEXT_CONTROL_CHECK ) {
+            control.setBackgroundResource(R.drawable.ic_check);
+            control.setOnClickListener(null);
+            params.setMarginEnd(m_editTextControlMarginEnd*2);
+            control.setLayoutParams(params);
+        }
     }
 
     private void tryLogin() {
